4acfbe9e74053f4d4bca7ecfbfd4f22a1eebe806
HoraceYim <hyim@nncsu.edu>
2015-09-11 23:35:51 -0400

    Fixes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,28 @@
+import java.util.*;
+
 /**
- * This is my first program.
+ * This is my first program with a few updates.
  * My name is Horace Yim.
+ * @author Horace Yim
  */
 public class HelloWorld {
    /**
-    * Main method and print.
+    * Says hello world and greets the user.
     * @param args command line arguments (not used)
     */
    public static void main(String[] args) {
-      // print statements
+
+      // Asks for the user's name.
+      Scanner console = new Scanner(System.in);
+      System.out.print("What is your name? ");
+
+      /** Saves the user's name as the variable text. */
+      String text = console.nextLine();
+
+      // Prints Hello statements.
+      System.out.println();
       System.out.println("Hello world!");
+      System.out.println("Hello " + text + " !");
       System.out.println("Let's have fun learning Java!");
    }
 }
