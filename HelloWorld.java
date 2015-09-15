900b40d9b6aa7fa52b4a6c6dd9a5d8d441c5b20c
HoraceYim <hyim@nncsu.edu>
2015-09-14 23:18:40 -0400

    Hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -23,6 +23,26 @@ public class HelloWorld {
       System.out.println();
       System.out.println("Hello world!");
       System.out.println("Hello " + text + " !");
+
+      // Declare variable for reverse stirng
+      String textReverse = "";
+
+      /**
+       * The length of the user's name.
+       */
+      int length = text.length();
+
+      // Reverse string for the user's name
+      for (int i = length - 1; i >= 0; i--) {
+         textReverse = textReverse + text.charAt(i);
+      }
+
+      // Prints reverse string.
+      System.out.println("! " + textReverse + " olleH");
+
+      // Prints fun statements.
       System.out.println("Let's have fun learning Java!");
+      System.out.println();
+
    }
 }
